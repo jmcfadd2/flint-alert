605ac6613bc442db7a769d1f17ec0ea6574aaab6
jmcfadd <justinmcfadd4@gmail.com>
2021-02-13 15:15:54 -0500

    feat: Added Mobile Sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import { CloseIcon, Icon, SidebarContainer, SidebarLink, SidebarMenu, SidebarRoute, SidebarWrapper, SideBtnWrap } from './SidebarElements'
 
-export default function Sidebar() {
+export default function Sidebar({ toggle, isOpen }) {
   return (
-    <SidebarContainer>
-      <Icon>
+    <SidebarContainer isOpen={isOpen} onClick={toggle}>
+      <Icon onClick={toggle}>
         <CloseIcon />
       </Icon>
       <SidebarWrapper>
         <SidebarMenu>
-          <SidebarLink to='about'>About</SidebarLink>
-          <SidebarLink to='discover'>Discover</SidebarLink>
-          <SidebarLink to='resources'>Resources</SidebarLink>
+          <SidebarLink to='about' onClick={toggle}>About</SidebarLink>
+          <SidebarLink to='discover' onClick={toggle}>Discover</SidebarLink>
+          <SidebarLink to='resources' onClick={toggle}>Resources</SidebarLink>
         </SidebarMenu>
         <SideBtnWrap>
-          <SidebarRoute to='/contact'>Contact Us</SidebarRoute>
+          <SidebarRoute to='/contact' onClick={toggle}>Contact Us</SidebarRoute>
         </SideBtnWrap>
       </SidebarWrapper>
     </SidebarContainer>
